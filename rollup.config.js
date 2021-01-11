52e4e5f4c0e35d39fe9919e6b2236715b7daecab
Robert Ing <robertming@gmail.com>
2021-01-11 15:48:28 -0500

    Update rollup.config.js to remove build file from root directory (#12)

--- rollup.config.js
+++ rollup.config.js
@@ -5,22 +5,6 @@ export default [
     {
         input: 'src/SimpleReach.js',
         output: {
-            file: 'SimpleReach.js',
-            format: 'iife',
-            exports: 'named',
-            name: 'mpSimpleReachKit',
-            strict: false
-        },
-        plugins: [
-            resolve({
-                browser: true
-            }),
-            commonjs()
-        ]
-    },
-    {
-        input: 'src/SimpleReach.js',
-        output: {
             file: 'dist/SimpleReach.iife.js',
             format: 'iife',
             exports: 'named',
